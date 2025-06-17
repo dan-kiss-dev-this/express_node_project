3a89b72dc881eaac1c994f5b30962eb195d0df90
Daniel Kiss <flowfast47@gmail.com>
2025-06-17 16:25:02 -0700

    add POST endpoint

--- server.js
+++ server.js
@@ -15,13 +15,32 @@ const friends = [
     }
 ]
 
+app.use(express.json())
+
 app.use((req, res, next) => {
     const start = Date.now();
-    console.log(req.method, req.url)
+
     next();
     // actions go here, come back here on the returned response
     const delta = Date.now() - start
     console.log('delta', delta)
+})
+
+app.post('/friends', (req, res) => {
+    // note servers dont understand json automatically, got to parse with express.json() middleware function, note this accounts for Content-Type being application/json
+    // console.log(req.method, req.url, req.body)
+
+    // note the use of return here to stop additional code execution as if there is not a name we want to send the error back and not have a friend object tried to get made as the name property is blank and that will be a different error, we even check if the req.body is included and not blank otherwise its an error
+    if (!req.body || !req.body.name) {
+        return res.status(400).json({
+            error: 'Friend did not have name'
+        })
+    }
+    const newFriend = {
+        name: req.body.name,
+        id: friends.length
+    }
+    res.json(newFriend)
 })
 
 app.get('/friends', (req, res) => {

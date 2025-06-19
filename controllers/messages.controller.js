95ec915c4ebb9dd51f2e63e4dfac5a629f21b9d7
Daniel Kiss <flowfast47@gmail.com>
2025-06-18 17:43:37 -0700

    send image with express

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,6 +1,12 @@
+const path = require('path')
+
 // use function instead of variable so node can point to the function for debugging
 function getMessages(req, res) {
-    res.send('<ul><li>Hello Albert!</li></ul>')
+    // note we need absolute path from "/" machine root directory
+    const absolutePath = path.join(__dirname, '..', 'public', 'skier.jpg')
+    // res.sendFile() from express
+    res.sendFile(absolutePath)
+    // res.send('<ul><li>Hello Albert!</li></ul>')
 }
 
 function postMessages(req, res) {
